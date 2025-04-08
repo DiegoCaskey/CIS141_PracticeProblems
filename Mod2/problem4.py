23d9a2369b5b0c0371bbf4566317b776acd74a71
DiegoCaskey <diego.caskey@icloud.com>
2025-04-08 14:05:26 -0700

    Create problem4.py

---
+++ Mod2/problem4.py
@@ -0,0 +1,4 @@
+birth = int(input("What year were you born? "))
+current = int(input("What year is it right now? "))
+age = current - birth
+print(f"You are {age} years old if you have already had your birthday this year!")
