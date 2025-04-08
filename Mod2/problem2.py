055b905a49aeaa5ba34b2159adede66140ae1f41
DiegoCaskey <diego.caskey@icloud.com>
2025-04-08 13:40:54 -0700

    Create problem2.py

---
+++ Mod2/problem2.py
@@ -0,0 +1,10 @@
+num_1 = float(input("Give me a number: "))
+num_2 = float(input("Give me another number: "))
+answer_1 = num_1 + num_2
+answer_2 = num_1 - num_2
+answer_3 = num_1 * num_2
+answer_4 = num_1 / num_2
+print(f"{num_1} plus {num_2} equals {answer_1}")
+print(f"{num_1} minus {num_2} equals {answer_2}")
+print(f"{num_1} times {num_2} equals {answer_3}")
+print(f"{num_1} divided by {num_2} equals {answer_4:.3f}")
