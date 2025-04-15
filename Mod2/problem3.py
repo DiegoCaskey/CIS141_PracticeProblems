9adff83e63ed0b24957c83f7b01976aec00195bc
DiegoCaskey <diego.caskey@icloud.com>
2025-04-15 13:02:22 -0700

    Update problem3.py

--- Mod2/problem3.py
+++ Mod2/problem3.py
@@ -1,3 +1,4 @@
+# 3. Create a program that prompts for the side lengths of a triangle and computes the area using Heron's formula. (https://en.wikipedia.org/wiki/Heron%27s_formula)
 # This calculator will provide an error if the values of a, b, or c do not properly satisfy Heron's formula.
 import math
 a = float(input("What is side A?: "))
